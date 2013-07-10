7db58bab10c436727dd3c3506be6408d2f3f8f97
Andreas Schultz <aschultz@tpip.net>
2013-07-10 16:49:15 +0200

    remove obsolete typedefs

--- smac_code.h
+++ smac_code.h
@@ -23,9 +23,6 @@
  *******************************************************************************************/
 
 #include <linux/types.h>
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
 
 enum {
 	ERROR = 0,
